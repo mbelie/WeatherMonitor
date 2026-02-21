06e52cc38813bbaabe92882c846a6de03c3ed46e
mbelie <mbelie@tripleringtech.com>
2026-02-21 10:47:02 -0600

    Update enums.py

--- device/enums.py
+++ device/enums.py
@@ -1,5 +1,9 @@
 from enum import Enum
 
 class TemperatureUnit(Enum):
+    """Enumeration of supported temperature units."""
     Fahrenheit = 1
     Celsius = 2
+
+    def to_string(self) -> str:
+        return self.name
